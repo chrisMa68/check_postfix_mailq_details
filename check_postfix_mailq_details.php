85e62ab24a7930d41df27f68b3e632372eac5ea7
chrisMa68 <info@chwieser.de>
2016-05-24 15:40:57 +0200

    Update check_postfix_mailq_details.php

--- check_postfix_mailq_details.php
+++ check_postfix_mailq_details.php
@@ -2,6 +2,22 @@
 #
 # Angepasst 
 # Plugin: check_postfix_mailq_detail
+# Copyright 2016, Christopher Wieser
+# 
+# Created: 2016-05  (mail@opso-it.de)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 $opt[1] = "--vertical-label Mailq -l0  --title \"Mailq for $hostname / $servicedesc\" ";
 #
