c6a17566eeb955a7ea778d8f363919b54006953e
chrisMa68 <info@chwieser.de>
2016-06-06 15:45:32 +0200

    Update check_postfix_mailq_details.sh

--- check_postfix_mailq_details.sh
+++ check_postfix_mailq_details.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
-# 23.3.2016 chw @ wkiv050
+# Plugin: check_postfix_mailq_detail
+# Copyright 2016, Christopher Wieser
+# 
+# Created: 2016-06  (mail@opso-it.de)
 #
-#Variablen festlegen
-. /usr/lib/nagios/plugins/utils.sh
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#Define Variables
 Version="0.5"
+. /usr/lib/nagios/plugins/utils.sh
 dir_log=/var/log/postfix
 dir_post=/var/spool/postfix
 # Chose the qshape location
@@ -14,14 +30,16 @@ programm=/usr/share/doc/packages/postfix-doc/auxiliary/qshape/qshape.pl
 ##Test Permissions
 	if  [ ! -r $dir_post/active ]
 	then
-		echo "Insufficient Permissions to "$dir_post"active" /n
-	        echo "Fix the Permissions or use the Script XXX every 5 Miniutes as a Cronjob" 	
-		exit 1
+		echo "Insufficient Permissions to "$dir_post"active" 
+	        echo "Fix the Permissions or use the Script mailq_details.sh as a Cronjob"
+	        echo "Do you use the Script mailq_details.sh, then disable this test and the 
+	        Lines 46,47 and 48 and activate the lines 50,51,52"
+		exit 2
 	fi
 	if [ ! -x $programm ]
 	then
-	        echo "qshape.pl dosen't exist. Install postfix-doc and set the x-bit to qshape.pl"
-		exit 1	
+	        echo "qshape dosen't exist. Install postfix-doc and set the x-bit to qshape"
+		exit 2	
        	fi
 
 #Monitoring user has access to /var/spool/postfix/active
@@ -32,32 +50,24 @@ postfix_incoming=`$programm $dir_post/incoming | awk ' $1 ~ "TOTAL" {print $2 } 
 #postfix_active=`cat $dir_log/active`
 #postfix_deferred=`cat $dir_log/deferred`
 #postfix_incoming=`cat $dir_log/incoming`
-#
-queue_id='^[A-F0-9][A-F0-9][A-F0-9][A-F0-9][A-F0-9][A-F0-9][A-F0-9][A-F0-9][A-F0-9][A-F0-9]'
-qsize=$(mailq | egrep -c $queue_id)
-
-
-
 ##################################
 EXIT_STATUS=$STATE_OK
 #########################
-#define your values
+#Define your Warn & Critical values
 ##########################
-warn_total=1
-warn_incoming=1
-warn_deferred=1
-warn_active=1
-crit_total=1000
+warn_total=1500
+warn_incoming=500
+warn_deferred=500
+warn_active=500
+crit_total=3000
 crit_incoming=1000
 crit_deferred=1000
 crit_active=1000
-##############################
-###
 ## Print usage
 usage() {
-		echo " check_postfixdetailqueue $VERSION - Monitoring Postfix  mail queue check script"
+		echo " check_postfix_detail_details $VERSION - Monitoring Postfix  mail queue check script"
 		echo ""
-		echo " Usage: check_postfixmailqueue "
+		echo " Usage: check_postfix_mailq_details "
 		echo " Please set the values for Warning and Critical in the script"
 		echo ""
 	}
