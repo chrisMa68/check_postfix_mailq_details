ba9e8651004315e0e99f253d6b450197c838807d
chrisMa68 <info@chwieser.de>
2016-06-06 15:28:30 +0200

    Update mailq_details.sh

--- mailq_details.sh
+++ mailq_details.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+# Plugin: check_postfix_mailq_detail
+# Copyright 2016, Christopher Wieser
+# 
+# Created: 2016-06  (mail@opso-it.de)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 dir_post=/var/spool/postfix
 dir_log=/var/log/postfix
 
